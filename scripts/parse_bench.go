55a92b514fa09c711808de36011aa0cdeaac41ba
Adel Hadadi <hadadiadel81@gmail.com>
2025-10-20 16:24:26 +0330

    fix: add missing metrics

--- scripts/parse_bench.go
+++ scripts/parse_bench.go
@@ -13,9 +13,14 @@ import (
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 )
 
-var benchRe = regexp.MustCompile(`^(Benchmark\S+)\s+(\d+)\s+([\d\.]+)\s+ns/op`)
+var benchRe = regexp.MustCompile(
+	`^(Benchmark\S+)\s+(\d+)\s+([\d\.]+)\s+ns/op\s+(\d+)\s+B/op\s+(\d+)\s+allocs/op`,
+)
 
 func main() {
+	commit := os.Getenv("GITHUB_SHA")
+	branch := os.Getenv("GITHUB_REF_NAME")
+
 	file, err := os.Open("bench.txt") // file with `go test -bench=. -benchmem` output
 	if err != nil {
 		panic(err)
@@ -35,19 +40,28 @@ func main() {
 		line := scanner.Text()
 		if strings.HasPrefix(line, "Benchmark") {
 			m := benchRe.FindStringSubmatch(line)
-			if len(m) == 4 {
+			if len(m) == 6 {
 				name := m[1]
 				iters, _ := strconv.ParseInt(m[2], 10, 64)
 				nsPerOp, _ := strconv.ParseFloat(m[3], 64)
+				bPerOp, _ := strconv.ParseInt(m[4], 10, 64)
+				allocsPerOp, _ := strconv.ParseInt(m[5], 10, 64)
 
-				fmt.Printf("Parsed: %s iters=%d ns/op=%.3f\n", name, iters, nsPerOp)
+				fmt.Printf("✅ Parsed: %-40s | iters=%d ns/op=%.3f B/op=%d allocs/op=%d\n",
+					name, iters, nsPerOp, bPerOp, allocsPerOp)
 
 				point := influxdb2.NewPoint(
 					"benchmark",
-					map[string]string{"name": name},
+					map[string]string{
+						"name":   name,
+						"branch": branch,
+						"commit": commit,
+					},
 					map[string]interface{}{
 						"iterations": iters,
-						"ns_per_op":  nsPerOp,
+						"time_ns":    nsPerOp,
+						"bytes":      bPerOp,
+						"allocs":     allocsPerOp,
 					},
 					time.Now(),
 				)
